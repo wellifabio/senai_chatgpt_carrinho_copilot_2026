87f6a1fe9e27a89d5b20a3f54ac5d375f7bb2ef4
wellifabio <wellifabio@hotmail.com>
2026-08-01 11:52:57 -0300

    arquivos

---
+++ script.js
@@ -0,0 +1,138 @@
+const produtos = []
+const carrinho = []
+
+const btnCarrinho = document.querySelector('#btnCarrinho')
+const modalCarrinho = document.querySelector('#modalCarrinho')
+const listaCarrinho = document.querySelector('#listaCarrinho')
+const btnFecharModal = document.querySelector('#btnFecharModal')
+const btnEnviarPedido = document.querySelector('#btnEnviarPedido')
+
+inicializar()
+
+async function inicializar() {
+    await obterProdutos()
+    renderProdutos()
+    configurarEventosCarrinho()
+}
+
+async function obterProdutos() {
+    const response = await fetch('data.json')
+    const data = await response.json()
+    produtos.push(...data.produtos)
+}
+
+function renderProdutos() {
+    const main = document.querySelector('main')
+    main.innerHTML = ''
+    produtos.forEach((p, index) => {
+        main.innerHTML += `<div>
+            <h2>${p.nome}</h2>
+            <p>${p.descricao}</p>
+            <p>R$ ${p.preco.toFixed(2)}</p>
+            <img src="${p.img}" alt="${p.nome}">
+            <button class="btn-add-carrinho" data-index="${index}">Add ao carrinho</button>
+        </div>`
+    })
+
+    const botoesAdicionar = document.querySelectorAll('.btn-add-carrinho')
+    botoesAdicionar.forEach(botao => {
+        botao.addEventListener('click', () => {
+            const indice = Number(botao.dataset.index)
+            adicionarAoCarrinho(indice)
+        })
+    })
+}
+
+function configurarEventosCarrinho() {
+    btnCarrinho.addEventListener('click', abrirModalCarrinho)
+    btnFecharModal.addEventListener('click', fecharModalCarrinho)
+    btnEnviarPedido.addEventListener('click', enviarPedido)
+    modalCarrinho.addEventListener('click', evento => {
+        if (evento.target === modalCarrinho) {
+            fecharModalCarrinho()
+        }
+    })
+}
+
+function adicionarAoCarrinho(indiceProduto) {
+    const itemExistente = carrinho.find(item => item.indiceProduto === indiceProduto)
+
+    if (itemExistente) {
+        itemExistente.quantidade += 1
+    } else {
+        carrinho.push({
+            indiceProduto,
+            produto: produtos[indiceProduto],
+            quantidade: 1
+        })
+    }
+
+    abrirModalCarrinho()
+}
+
+function abrirModalCarrinho() {
+    renderCarrinho()
+    modalCarrinho.classList.remove('oculto')
+}
+
+function fecharModalCarrinho() {
+    modalCarrinho.classList.add('oculto')
+}
+
+function renderCarrinho() {
+    listaCarrinho.innerHTML = ''
+
+    if (carrinho.length === 0) {
+        listaCarrinho.innerHTML = '<li>Carrinho vazio.</li>'
+        return
+    }
+
+    carrinho.forEach(item => {
+        listaCarrinho.innerHTML += `<li class="item-carrinho">
+            <div class="item-carrinho-topo">
+                <span>${item.produto.nome}</span>
+                <strong>R$ ${item.produto.preco.toFixed(2)}</strong>
+            </div>
+            <div class="controle-quantidade">
+                <button class="btn-qtd" data-acao="diminuir" data-index="${item.indiceProduto}">-</button>
+                <span class="qtd-valor">${item.quantidade}</span>
+                <button class="btn-qtd" data-acao="aumentar" data-index="${item.indiceProduto}">+</button>
+            </div>
+        </li>`
+    })
+}
+
+listaCarrinho.addEventListener('click', evento => {
+    const botao = evento.target.closest('.btn-qtd')
+
+    if (!botao) {
+        return
+    }
+
+    const indiceProduto = Number(botao.dataset.index)
+    const acao = botao.dataset.acao
+    const delta = acao === 'aumentar' ? 1 : -1
+    alterarQuantidadeItem(indiceProduto, delta)
+})
+
+function alterarQuantidadeItem(indiceProduto, delta) {
+    const item = carrinho.find(produto => produto.indiceProduto === indiceProduto)
+
+    if (!item) {
+        return
+    }
+
+    item.quantidade += delta
+
+    if (item.quantidade <= 0) {
+        const indiceItem = carrinho.findIndex(produto => produto.indiceProduto === indiceProduto)
+        carrinho.splice(indiceItem, 1)
+    }
+
+    renderCarrinho()
+}
+
+function enviarPedido() {
+    carrinho.length = 0
+    fecharModalCarrinho()
+}
